6fe4b26534b5aacb154351e5c0e9853d0c583d70
mtaham84 <mtaha2005b@gmail.com>
2026-07-11 05:54:41 -0400

    redirect ok

--- brand_site/urls.py
+++ brand_site/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),  # زبان گزین
@@ -11,6 +12,8 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('sr8405/', admin.site.urls),
     path('', include('brands.urls')),
+    path('catalog/', lambda request: redirect('/')),
+    path('catalog', lambda request: redirect('/')),
 )
 
 if settings.DEBUG:
